4585f55837331f0dcf8030d05f24ed01846b44cb
mdaashir <s.mohamedaashir@gmail.com>
2024-06-26 00:44:17 +0530

    Temperature commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world !");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Fahrenheit: ");
+        float Fahrenheit = input.nextFloat();
+        System.out.printf("Fahrenheit = %.2f F\n", Fahrenheit);
+        float Celsius = (Fahrenheit - 32) * (5.0f / 9.0f);
+        System.out.printf("Celsius = %.2f C\n", Celsius);
+        input.close();
     }
 }
