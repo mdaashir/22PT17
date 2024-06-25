d78b18f6de89f1c9ddcad98c0c720228b927a60a
mdaashir <s.mohamedaashir@gmail.com>
2024-06-26 00:47:16 +0530

    prg 1 commit

--- Main.java
+++ Main.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Temperature();
+    }
+    private static void Temperature() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Fahrenheit: ");
+        float Fahrenheit = input.nextFloat();
+        System.out.printf("Fahrenheit = %.2f F\n", Fahrenheit);
+        float Celsius = (Fahrenheit - 32) * (5.0f / 9.0f);
+        System.out.printf("Celsius = %.2f C\n", Celsius);
+        input.close();
     }
 }
