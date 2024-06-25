30ac0bfa23a2b4cc77a89ad8ccfd606492855f5f
mdaashir <s.mohamedaashir@gmail.com>
2024-06-26 00:52:33 +0530

    Distance

--- Hello.java
+++ Hello.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Hello {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the Fahrenheit: ");
-        float Fahrenheit = input.nextFloat();
-        System.out.printf("Fahrenheit = %.2f F\n", Fahrenheit);
-        float Celsius = (Fahrenheit - 32) * (5.0f / 9.0f);
-        System.out.printf("Celsius = %.2f C\n", Celsius);
+        System.out.print("Enter the Kilometers: ");
+        float KiloMeter = input.nextFloat();
+        System.out.printf("Kilometers = %.2f KM\n", KiloMeter);
+        float Mile = (0.62f * KiloMeter);
+        System.out.printf("Miles = %.2f mile\n", Mile);
         input.close();
     }
 }
