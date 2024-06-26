df2d428a58322dbe75522c0eb6e9a9f6818710d4
mdaashir <s.mohamedaashir@gmail.com>
2024-06-26 07:01:59 +0530

    Interest

--- Hello.java
+++ Hello.java
@@ -2,11 +2,18 @@ import java.util.Scanner;
 
 public class Hello {
     private static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.print("Enter the Kilometers: ");
-        float KiloMeter = input.nextFloat();
-        System.out.printf("Kilometers = %.2f KM\n", KiloMeter);
-        float Mile = (0.62f * KiloMeter);
-        System.out.printf("Miles = %.2f mile\n", Mile);
+        System.out.print("Enter the Principal, Time Period, Rate of Interest: ");
+        float Principal = input.nextFloat();
+        float NumberOfYear = input.nextFloat();
+        float RateOfInterest = input.nextFloat();
+        System.out.printf("Principal = Rs. %.2f\n", Principal);
+        System.out.printf("Time Period = %.2f Year(s)\n", NumberOfYear);
+        System.out.printf("Rate of Interest = %.2f %%\n", RateOfInterest);
+        float SI = (Principal * NumberOfYear * RateOfInterest) / 100;
+        float CI = (Principal * (float)Math.pow((1 + (RateOfInterest/100)), NumberOfYear)) - Principal;
+        System.out.printf("Simple Interest = Rs. %.2f\n", SI);
+        System.out.printf("Compound Interest = Rs. %.2f\n", CI);
     }
 }
