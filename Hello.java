5069111e337e46102b382ee308440c8bfe6380dd
mdaashir <s.mohamedaashir@gmail.com>
2024-06-26 06:26:10 +0530

    Declared input as global

--- Hello.java
+++ Hello.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 
 public class Hello {
+    private static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         System.out.print("Enter the Kilometers: ");
         float KiloMeter = input.nextFloat();
         System.out.printf("Kilometers = %.2f KM\n", KiloMeter);
         float Mile = (0.62f * KiloMeter);
         System.out.printf("Miles = %.2f mile\n", Mile);
-        input.close();
     }
 }
