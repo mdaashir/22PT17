044477bdaaab0bc3bd0d051bbf93caee85a3750e
mdaashir <s.mohamedaashir@gmail.com>
2024-06-27 07:07:05 +0530

    Lightning

--- Hello.java
+++ Hello.java
@@ -4,11 +4,12 @@ public class Hello {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.print("Enter the Distance between two cities(in Km): ");
-        float Kilometers = input.nextFloat();
-        float Miles = (0.621371f * Kilometers);
-        float Feet = (3280.84f * Kilometers);
-        System.out.printf("Distance between two cities = %.2f Km or %.2f miles or %.2f ft\n", Kilometers, Miles, Feet);
+        System.out.print("Enter the time elapsed(in sec): ");
+        float TimeElapsed = input.nextFloat();
+        float SpeedOfLight = 1100; // ft per sec
+        float Feet = (TimeElapsed * SpeedOfLight); // ft
+        float Miles = (Feet / 5280f); // ft to mile
+        System.out.printf("The lightning strike is approximately %.2f miles away.\n", Miles);
         System.out.println();
     }
 }
