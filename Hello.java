f2891850bcb2928c7361e1d23db95c84b8a19337
mdaashir <s.mohamedaashir@gmail.com>
2024-06-27 07:35:07 +0530

    slope

--- Hello.java
+++ Hello.java
@@ -4,12 +4,15 @@ public class Hello {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.print("Enter the time elapsed(in sec): ");
-        float TimeElapsed = input.nextFloat();
-        float SpeedOfLight = 1100; // ft per sec
-        float Feet = (TimeElapsed * SpeedOfLight); // ft
-        float Miles = (Feet / 5280f); // ft to mile
-        System.out.printf("The lightning strike is approximately %.2f miles away.\n", Miles);
-        System.out.println();
+        System.out.print("Enter the coordinates of the point (x1, y1): ");
+        float[] Point1 = { input.nextFloat(), input.nextFloat() };
+        System.out.print("Enter the coordinates of the point (x2, y2): ");
+        float[] Point2 = { input.nextFloat(), input.nextFloat() };
+        if (Point1[0] == Point2[0])
+            System.out.println("The points are on the same vertical line.");
+        else {
+            float Slope = (Point2[1] - Point1[1]) / (Point2[0] - Point1[0]);
+            System.out.printf("The slope of the line = %.2f\n", Slope);
+        }
     }
 }
