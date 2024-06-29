a816ceaa9ab5cbe2c397d9cfae525901701f1efb
mdaashir <s.mohamedaashir@gmail.com>
2024-06-29 15:45:20 +0530

    Weight

--- Hello.java
+++ Hello.java
@@ -4,15 +4,12 @@ public class Hello {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.print("Enter the coordinates of the point (x1, y1): ");
-        float[] Point1 = { input.nextFloat(), input.nextFloat() };
-        System.out.print("Enter the coordinates of the point (x2, y2): ");
-        float[] Point2 = { input.nextFloat(), input.nextFloat() };
-        if (Point1[0] == Point2[0])
-            System.out.println("The points are on the same vertical line.");
-        else {
-            float Slope = (Point2[1] - Point1[1]) / (Point2[0] - Point1[0]);
-            System.out.printf("The slope of the line = %.2f\n", Slope);
-        }
+        System.out.print("Enter the number of Hydrogen(H), Carbon(C), Oxygen(O) atoms: ");
+        int hydrogenAtoms = input.nextInt();
+        int carbonAtoms = input.nextInt();
+        int oxygenAtoms = input.nextInt();
+        float molecularWeight = (1.0079f * hydrogenAtoms) + (12.011f * carbonAtoms) + (15.9994f * oxygenAtoms);
+        System.out.printf("The molecular weight of %dH %dC %dO = %.2f grams/mole\n",hydrogenAtoms, carbonAtoms, oxygenAtoms, molecularWeight );
+        System.out.println();
     }
 }
