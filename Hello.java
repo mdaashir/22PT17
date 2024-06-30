6a58c7583dfff4de2a43248dbc0a1942c20a5dec
mdaashir <s.mohamedaashir@gmail.com>
2024-06-30 10:11:54 +0530

    Population

--- Hello.java
+++ Hello.java
@@ -4,12 +4,16 @@ public class Hello {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.print("Enter number of eggs: ");
-        int NumberOfEggs = input.nextInt();
-        int NumberofDozens = NumberOfEggs / 12;
-        int NumberofLeftOver = NumberOfEggs % 12;
-        float Price = (3.25f * NumberofDozens) + (0.45f * NumberofLeftOver);
-        System.out.printf("Total price of eggs = $%.2f ",Price);
+        System.out.print("Enter the current world population: ");
+        long CurrentPopulation = input.nextLong();
+        System.out.print("Enter the annual world population growth rate (as a percentage): ");
+        float GrowthRate = input.nextFloat();
+        GrowthRate /= 100;
+        System.out.println("Estimated World Population: ");
+        for (int year = 1; year <= 5; year++) {
+            double EstimatedPopulation = CurrentPopulation * Math.pow(1 + GrowthRate, year);
+            System.out.printf("Year %d: %.0f\n", year, EstimatedPopulation);
+        }
         System.out.println();
     }
 }
