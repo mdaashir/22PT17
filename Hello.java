88c810195c0bca86922590c02b6dfec167b2e72e
mdaashir <s.mohamedaashir@gmail.com>
2024-06-30 17:43:51 +0530

    Bmi

--- Hello.java
+++ Hello.java
@@ -4,20 +4,14 @@ public class Hello {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.print("Enter the monthly savings amount: ");
-        float SavingsAmount = input.nextFloat();
-        // System.out.print("Enter the annual interest rate: ");
-        // float AnnualRate = input.nextFloat() / 100f;
-        float AnnualRate = 5 / 100f;
-        float MonthlyRate = AnnualRate / 12f;
-        Double AmountValue = 0.0;
-        // System.out.print("Enter the number of months: ");
-        // int Months = input.nextInt();
-        int Months = 6;
-        for (int i = 0; i < Months; i++) {
-            AmountValue = (SavingsAmount + AmountValue) * (1 + MonthlyRate);
-        }
-        System.out.printf("Your savings amount after %d months = $%.3f",Months, AmountValue);
+        System.out.print("Enter the weight(in pounds): ");
+        float Pounds = input.nextFloat();
+        System.out.print("Enter the Height(in inches): ");
+        float Inches = input.nextFloat();
+        float Weight = 0.45359237f * Pounds; // kilograms
+        float Height = 0.0254f * Inches; // meters
+        float BMI = Weight / (Height * Height);
+        System.out.printf("BMI: %.2f", BMI);
         System.out.println();
     }
 }
