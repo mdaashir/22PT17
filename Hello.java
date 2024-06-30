51dd3eea5ec6b863dd6fdad1651894879deb1edd
mdaashir <s.mohamedaashir@gmail.com>
2024-06-30 18:09:09 +0530

    WindChill

--- Hello.java
+++ Hello.java
@@ -4,14 +4,21 @@ public class Hello {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.print("Enter the weight(in pounds): ");
-        float Pounds = input.nextFloat();
-        System.out.print("Enter the Height(in inches): ");
-        float Inches = input.nextFloat();
-        float Weight = 0.45359237f * Pounds; // kilograms
-        float Height = 0.0254f * Inches; // meters
-        float BMI = Weight / (Height * Height);
-        System.out.printf("BMI: %.2f", BMI);
+        System.out.print("Enter the temperature in Fahrenheit (between -58°F and 41°F): ");
+        float Temperature = input.nextFloat();
+        while (Temperature < -58 || Temperature > 41) {
+            System.out.print("Invalid input. Please enter a temperature between -58°F and 41°F: ");
+            Temperature = input.nextFloat();
+        }
+        System.out.print("Enter the wind speed in miles per hour (>= 2 mph): ");
+        float WindSpeed = input.nextFloat();
+        while (WindSpeed < 2) {
+            System.out.print("Invalid input. Please enter a wind speed of at least 2 mph: ");
+            WindSpeed = input.nextFloat();
+        }
+        double WindChill = 35.74f + 0.6215f * Temperature - 35.75f * Math.pow(WindSpeed, 0.16f)
+                + 0.4275f * Temperature * Math.pow(WindSpeed, 0.16f);
+        System.out.printf("The wind-chill temperature is: %.2f°F", WindChill);
         System.out.println();
     }
 }
