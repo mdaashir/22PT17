63b5e1462f06f8c8fd52d7572386c8f5f102cf1a
mdaashir <s.mohamedaashir@gmail.com>
2024-06-30 09:51:50 +0530

    Egg

--- Hello.java
+++ Hello.java
@@ -4,23 +4,12 @@ public class Hello {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        // System.out.print("Enter number of racers: ");
-        // int NumberOfRacers = input.nextInt();
-        int NumberOfRacers = 5;
-        int[] SpeedOfRacers = new int[NumberOfRacers];
-        int Average = 0;
-        System.out.print("Enter the speed of " + NumberOfRacers + " racer: ");
-        for (int i = 0; i < NumberOfRacers; i++) {
-            SpeedOfRacers[i] = input.nextInt();
-            Average += SpeedOfRacers[i];
-        }
-        Average /= NumberOfRacers;
-        // System.out.println("Average speed: " + Average + " kmph");
-        System.out.println("Speed of qualifying racers: ");
-        for (int i = 0; i < NumberOfRacers; i++) {
-            if (SpeedOfRacers[i] >= Average)
-                System.out.println("Racer " + (i + 1) + " = " + SpeedOfRacers[i] + " kmph");
-        }
+        System.out.print("Enter number of eggs: ");
+        int NumberOfEggs = input.nextInt();
+        int NumberofDozens = NumberOfEggs / 12;
+        int NumberofLeftOver = NumberOfEggs % 12;
+        float Price = (3.25f * NumberofDozens) + (0.45f * NumberofLeftOver);
+        System.out.printf("Total price of eggs = $%.2f ",Price);
         System.out.println();
     }
 }
