64d033c8772c937431f420f792d652ce0a75bd01
mdaashir <s.mohamedaashir@gmail.com>
2024-06-30 18:23:16 +0530

    Investment

--- Hello.java
+++ Hello.java
@@ -4,21 +4,14 @@ public class Hello {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.print("Enter the temperature in Fahrenheit (between -58°F and 41°F): ");
-        float Temperature = input.nextFloat();
-        while (Temperature < -58 || Temperature > 41) {
-            System.out.print("Invalid input. Please enter a temperature between -58°F and 41°F: ");
-            Temperature = input.nextFloat();
-        }
-        System.out.print("Enter the wind speed in miles per hour (>= 2 mph): ");
-        float WindSpeed = input.nextFloat();
-        while (WindSpeed < 2) {
-            System.out.print("Invalid input. Please enter a wind speed of at least 2 mph: ");
-            WindSpeed = input.nextFloat();
-        }
-        double WindChill = 35.74f + 0.6215f * Temperature - 35.75f * Math.pow(WindSpeed, 0.16f)
-                + 0.4275f * Temperature * Math.pow(WindSpeed, 0.16f);
-        System.out.printf("The wind-chill temperature is: %.2f°F", WindChill);
+        System.out.print("Enter the investment amount: ");
+        float InvestmentAmount = input.nextFloat();
+        System.out.print("Enter the annual interest rate: ");
+        float MonthlyRate = (input.nextFloat() / 100) / 12;
+        System.out.print("Enter the number of years: ");
+        int NumberOfMonths = input.nextInt() * 12;
+        double FutureValue = InvestmentAmount * Math.pow((1 + MonthlyRate), NumberOfMonths);
+        System.out.printf("The future investment amount = $%.2f", FutureValue);
         System.out.println();
     }
 }
