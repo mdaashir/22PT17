2469fe0c4526819c15f5eae32c59c85782946717
mdaashir <s.mohamedaashir@gmail.com>
2024-06-30 18:35:50 +0530

    Digits

--- Hello.java
+++ Hello.java
@@ -4,14 +4,20 @@ public class Hello {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.print("Enter the investment amount: ");
-        float InvestmentAmount = input.nextFloat();
-        System.out.print("Enter the annual interest rate: ");
-        float MonthlyRate = (input.nextFloat() / 100) / 12;
-        System.out.print("Enter the number of years: ");
-        int NumberOfMonths = input.nextInt() * 12;
-        double FutureValue = InvestmentAmount * Math.pow((1 + MonthlyRate), NumberOfMonths);
-        System.out.printf("The future investment amount = $%.2f", FutureValue);
+        System.out.print("Enter a five-digit number: ");
+        int Number = input.nextInt();
+        while (Number < 10000 || Number > 99999) {
+            System.out.print("Invalid input. Please enter a five-digit number: ");
+            Number = input.nextInt();
+        }
+        int[] Digits = new int[5];
+        for (int i = 0; i < 5; i++) {
+            Digits[i] = Number % 10;
+            Number /= 10;
+        }
+        for (int i : Digits) {
+            System.out.print(i + "\t");
+        }
         System.out.println();
     }
 }
