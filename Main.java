661f7b51aa6531521067c9e4038e93ccc136ea84
mdaashir <s.mohamedaashir@gmail.com>
2024-06-30 18:10:39 +0530

    WindChill commit

--- Main.java
+++ Main.java
@@ -21,6 +21,7 @@ public class Main {
         Driving();
         Savings();
         Bmi();
+        WindChill();
     }
 
     private static void Temperature() {
@@ -235,6 +236,25 @@ public class Main {
         float Height = 0.0254f * Inches; // meters
         float BMI = Weight / (Height * Height);
         System.out.printf("BMI: %.2f", BMI);
+        System.out.println();
+    }
+
+    public static void WindChill() {
+        System.out.print("Enter the temperature in Fahrenheit (between -58°F and 41°F): ");
+        float Temperature = input.nextFloat();
+        while (Temperature < -58 || Temperature > 41) {
+            System.out.print("Invalid input. Please enter a temperature between -58°F and 41°F: ");
+            Temperature = input.nextFloat();
+        }
+        System.out.print("Enter the wind speed in miles per hour (>= 2 mph): ");
+        float WindSpeed = input.nextFloat();
+        while (WindSpeed < 2) {
+            System.out.print("Invalid input. Please enter a wind speed of at least 2 mph: ");
+            WindSpeed = input.nextFloat();
+        }
+        double WindChill = 35.74f + 0.6215f * Temperature - 35.75f * Math.pow(WindSpeed, 0.16f)
+                + 0.4275f * Temperature * Math.pow(WindSpeed, 0.16f);
+        System.out.printf("The wind-chill temperature is: %.2f°F", WindChill);
         System.out.println();
     }
 }
