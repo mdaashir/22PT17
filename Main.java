118baf0c8a8bf0aafb521fcfd2ea9e7016a6a31b
mdaashir <s.mohamedaashir@gmail.com>
2024-06-30 18:37:43 +0530

    Digits commit

--- Main.java
+++ Main.java
@@ -23,6 +23,7 @@ public class Main {
         Bmi();
         WindChill();
         Investment();
+        Digits();
     }
 
     private static void Temperature() {
@@ -268,6 +269,24 @@ public class Main {
         int NumberOfMonths = input.nextInt() * 12;
         double FutureValue = InvestmentAmount * Math.pow((1 + MonthlyRate), NumberOfMonths);
         System.out.printf("The future investment amount = $%.2f", FutureValue);
+        System.out.println();
+    }
+
+    public static void Digits() {
+        System.out.print("Enter a five-digit number: ");
+        int Number = input.nextInt();
+        while (Number < 10000 || Number > 99999) {
+            System.out.print("Invalid input. Please enter a five-digit number: ");
+            Number = input.nextInt();
+        }
+        int[] Digits = new int[5];
+        for (int i = 0; i < 5; i++) {
+            Digits[i] = Number % 10;
+            Number /= 10;
+        }
+        for (int i : Digits) {
+            System.out.print(i + "\t");
+        }
         System.out.println();
     }
 }
