27713910816c3b0bdb65d70088264a753109b6c2
mdaashir <s.mohamedaashir@gmail.com>
2024-06-30 17:30:55 +0530

    Savings

--- Hello.java
+++ Hello.java
@@ -4,19 +4,20 @@ public class Hello {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.print("Enter the total miles driven per day: ");
-        float TotalMilesPerDay = input.nextFloat();
-        System.out.print("Enter the average miles per gallon: ");
-        float MilesPerGallon = input.nextFloat();
-        System.out.print("Enter the cost per gallon of gasoline: ");
-        float CostPerGallon = input.nextFloat();
-        float DailyGasolineCost = (TotalMilesPerDay / MilesPerGallon) * CostPerGallon;
-        System.out.print("Enter the parking fees per day: ");
-        float ParkingFees = input.nextFloat();
-        System.out.print("Enter the tolls per day: ");
-        float TollsPerDay = input.nextFloat();
-        float DailyDrivingCost = DailyGasolineCost + ParkingFees + TollsPerDay;
-        System.out.printf("Your daily driving cost is: $%.2f%n", DailyDrivingCost);
+        System.out.print("Enter the monthly savings amount: ");
+        float SavingsAmount = input.nextFloat();
+        // System.out.print("Enter the annual interest rate: ");
+        // float AnnualRate = input.nextFloat() / 100f;
+        float AnnualRate = 5 / 100f;
+        float MonthlyRate = AnnualRate / 12f;
+        Double AmountValue = 0.0;
+        // System.out.print("Enter the number of months: ");
+        // int Months = input.nextInt();
+        int Months = 6;
+        for (int i = 0; i < Months; i++) {
+            AmountValue = (SavingsAmount + AmountValue) * (1 + MonthlyRate);
+        }
+        System.out.printf("Your savings amount after %d months = $%.3f",Months, AmountValue);
         System.out.println();
     }
 }
