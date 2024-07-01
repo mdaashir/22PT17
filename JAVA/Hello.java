b18ff7a99d68e0b1d6599f248273641a251292e6
mdaashir <s.mohamedaashir@gmail.com>
2024-07-02 01:22:43 +0530

    Revert "Work Sheet 2 commit"
    This reverts commit 354d22f2491d7b8dab6929ed62dab202a1ce6d50.

--- JAVA/Hello.java
+++ JAVA/Hello.java
@@ -3,6 +3,7 @@ import java.util.function.IntUnaryOperator;
 
 public class Hello {
     private static Scanner input = new Scanner(System.in);
+
     private static IntUnaryOperator Function = null;
 
     public static void main(String[] args) {
