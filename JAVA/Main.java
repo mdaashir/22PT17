297f75c2e169b5f9ce1b2a0cafa2fcf3a99092d2
mdaashir <s.mohamedaashir@gmail.com>
2024-07-02 01:20:56 +0530

    Work Sheet 2 commit

--- JAVA/Main.java
+++ JAVA/Main.java
@@ -1,8 +1,15 @@
 import java.util.Scanner;
+import java.util.function.IntUnaryOperator;
 
 public class Main {
     private static Scanner input = new Scanner(System.in);
+    private static IntUnaryOperator Function = null;
 
     public static void main(String[] args) {
+        Factorial();
+    }
+
+    public static void Factorial() {
+
     }
 }
