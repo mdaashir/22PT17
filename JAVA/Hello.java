e5f8992b01fc387fcc7e6035dd62d6a807338a64
mdaashir <s.mohamedaashir@gmail.com>
2024-07-02 07:22:34 +0530

    changed to default value

--- JAVA/Hello.java
+++ JAVA/Hello.java
@@ -4,8 +4,9 @@ public class Hello {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.print("Enter a number: ");
-        int Number = input.nextInt();
+        // System.out.print("Enter a number: ");
+        // int Number = input.nextInt();
+        int Number = 100;
         long Sum = (Number * (Number + 1)) / 2;
         float Average = ((float) Sum / Number);
         System.out.printf("The sum of the first %d natural numbers is %d and average is %.2f", Number, Sum, Average);
