af04d7ad1bd5796b2ec8cf14314a7ecb481ec97c
mdaashir <s.mohamedaashir@gmail.com>
2024-07-02 06:37:53 +0530

    Solve commit

--- JAVA/Main.java
+++ JAVA/Main.java
@@ -7,13 +7,33 @@ public class Main {
 
     public static void main(String[] args) {
         Factorial();
+        Solve();
     }
 
     public static void Factorial() {
         System.out.print("Enter a number: ");
         int Number = input.nextInt();
         Function = n -> n == 0 ? 1 : n * Function.applyAsInt(n - 1);
-        System.out.print("Factorial of " + Number + " is " + Function.applyAsInt(Number));
+        System.out.println("Factorial of " + Number + " is " + Function.applyAsInt(Number));
+        System.out.println();
+    }
+
+    public static void Solve() {
+        System.out.print("Enter coefficient of A, B, C [in A(x^2) + B(x) + C]: ");
+        float[] Coefficient = { input.nextFloat(), input.nextFloat(), input.nextFloat() };
+        float Discriminant = (Coefficient[1] * Coefficient[1]) - (4 * Coefficient[0] * Coefficient[2]);
+        if (Discriminant > 0) {
+            float Root1 = (-Coefficient[1] + (float) Math.sqrt(Discriminant)) / (2 * Coefficient[0]);
+            float Root2 = (-Coefficient[1] - (float) Math.sqrt(Discriminant)) / (2 * Coefficient[0]);
+            System.out.print("The equation has two real and distinct roots: " + Root1 + " and " + Root2);
+        } else if (Discriminant == 0) {
+            float Root = -Coefficient[1] / (2 * Coefficient[0]);
+            System.out.print("The equation has one real root: " + Root);
+        } else {
+            float RealPart = -Coefficient[1] / (2 * Coefficient[0]);
+            float ImaginaryPart = (float) (Math.sqrt(-Discriminant) / (2 * Coefficient[0]));
+            System.out.print("The equation has complex roots: " + RealPart + " ± " + ImaginaryPart + "i");
+        }
         System.out.println();
     }
 }
