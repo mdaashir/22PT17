2ba222050cfc8a9b1b7b097b98a7366f63f47c07
mdaashir <s.mohamedaashir@gmail.com>
2024-07-02 06:51:59 +0530

    NumberName

--- JAVA/Hello.java
+++ JAVA/Hello.java
@@ -4,21 +4,28 @@ public class Hello {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.print("Enter coefficient of A, B, C [in A(x^2) + B(x) + C]: ");
-        float[] Coefficient = { input.nextFloat(), input.nextFloat(), input.nextFloat() };
-        float Discriminant = (Coefficient[1] * Coefficient[1]) - (4 * Coefficient[0] * Coefficient[2]);
-        if (Discriminant > 0) {
-            float Root1 = (-Coefficient[1] + (float) Math.sqrt(Discriminant)) / (2 * Coefficient[0]);
-            float Root2 = (-Coefficient[1] - (float) Math.sqrt(Discriminant)) / (2 * Coefficient[0]);
-            System.out.println("The equation has two real and distinct roots: " + Root1 + " and " + Root2);
-        } else if (Discriminant == 0) {
-            float Root = -Coefficient[1] / (2 * Coefficient[0]);
-            System.out.println("The equation has one real root: " + Root);
-        } else {
-            float RealPart = -Coefficient[1] / (2 * Coefficient[0]);
-            float ImaginaryPart = (float) (Math.sqrt(-Discriminant) / (2 * Coefficient[0]));
-            System.out.println("The equation has complex roots: " + RealPart + " ± " + ImaginaryPart + "i");
-        }
+        System.out.print("Enter a number: ");
+        int Number = input.nextInt();
+        if (Number == 1)
+            System.out.print("ONE");
+        else if (Number == 2)
+            System.out.print("TWO");
+        else if (Number == 3)
+            System.out.print("THREE");
+        else if (Number == 4)
+            System.out.print("FOUR");
+        else if (Number == 5)
+            System.out.print("FIVE");
+        else if (Number == 6)
+            System.out.print("SIX");
+        else if (Number == 7)
+            System.out.print("SEVEN");
+        else if (Number == 8)
+            System.out.print("EIGHT");
+        else if (Number == 9)
+            System.out.print("NINE");
+        else
+            System.out.print("NONE");
         System.out.println();
     }
 }
