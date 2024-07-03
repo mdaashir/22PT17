44853af7058f147913411f5d9368297501e28686
mdaashir <s.mohamedaashir@gmail.com>
2024-07-03 17:19:28 +0530

    Initial commit

---
+++ JAVA/ProblemSheet_2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.function.IntUnaryOperator;
+
+public class ProblemSheet_2 {
+    private static Scanner input = new Scanner(System.in);
+    private static IntUnaryOperator Function = null;
+
+    public static void main(String[] args) {
+        Factorial();
+    }
+
+    private static void Factorial() {
+        System.out.print("Enter a number: ");
+        int Number = input.nextInt();
+        Function = n -> n == 0 ? 1 : n * Function.applyAsInt(n - 1);
+        System.out.println("Factorial of " + Number + " is " + Function.applyAsInt(Number));
+        System.out.println();
+    }
+}
