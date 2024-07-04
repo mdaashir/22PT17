c294945abeb96e69dfab7801779eafe011293abd
mdaashir <s.mohamedaashir@gmail.com>
2024-07-04 07:06:00 +0530

    Formatted

--- DAA/WS1/p1.cpp
+++ DAA/WS1/p1.cpp
@@ -1,33 +1,40 @@
 #include <bits/stdc++.h>
 
-int main(){
-// Initialization of array
+int main()
+{
+	// Initialization of array
 	std::vector<int> arr;
 	int temp;
 	std::cout << "Enter the values(-1 to stop) " << std::endl;
-	while (temp != -1){
+	while (temp != -1)
+	{
 		std::cin >> temp;
-		if(temp != -1)
+		if (temp != -1)
 			arr.push_back(temp);
-		}
+	}
 	std::cout << "Values Successfully inputed" << std::endl;
-// Algorithm starts
+	// Algorithm starts
 	int fst = 0, snd = 1;
-	while (fst < snd && snd < arr.size()){
-		if(arr[fst] % 2 == 1 && arr[snd] % 2 == 0){
-		std::swap(arr[fst],arr[snd]);
-		fst++, snd++;
+	while (fst < snd && snd < arr.size())
+	{
+		if (arr[fst] % 2 == 1 && arr[snd] % 2 == 0)
+		{
+			std::swap(arr[fst], arr[snd]);
+			fst++, snd++;
 		}
-		else if(arr[fst] % 2 == 1 && arr[snd] % 2 == 1){
-		snd++;
+		else if (arr[fst] % 2 == 1 && arr[snd] % 2 == 1)
+		{
+			snd++;
 		}
-		else{
-		fst++,snd++;
+		else
+		{
+			fst++, snd++;
 		}
 	}
-// Display of array
+	// Display of array
 	std::cout << "Final array: " << std::endl;
-	for (auto i: arr){
+	for (auto i : arr)
+	{
 		std::cout << i << " ";
 	}
 	std::cout << std::endl;
