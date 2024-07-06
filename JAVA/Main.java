247533e77f5df5ba80cc1c6c85a4155da7674668
mdaashir <s.mohamedaashir@gmail.com>
2024-07-06 17:05:44 +0530

    Fibonacci commit

--- JAVA/Main.java
+++ JAVA/Main.java
@@ -10,6 +10,7 @@ public class Main {
         Solve();
         NumberName();
         SumAverage();
+        Fibonacci();
     }
 
     private static void Factorial() {
@@ -88,6 +89,22 @@ public class Main {
         long Sum = (Number * (Number + 1)) / 2;
         float Average = ((float) Sum / Number);
         System.out.printf("\nThe sum of the first %d natural numbers is %d and average is %.2f", Number, Sum, Average);
+        System.out.println();
+    }
+
+    public static void Fibonacci() {
+        // System.out.print("Enter a number: ");
+        // int Number = input.nextInt();
+        int Number = 20;
+        int[] Fibo = new int[Number];
+        Fibo[0] = 0;
+        Fibo[1] = 1;
+        Function = n -> (n < 2) ? Fibo[n] : (Fibo[n] = Function.applyAsInt(n - 1) + Function.applyAsInt(n - 2));
+        Function.applyAsInt(Number - 1);
+        int sum = 0;
+        for (int i : Fibo)
+            sum += i;
+        System.out.printf("The average of %d Fibonacci numbers is %f", Number, (float) sum / Number);
         System.out.println();
     }
 }
