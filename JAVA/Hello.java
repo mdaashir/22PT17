651a4b33b2d437df9b0c20008379428afb129ed4
mdaashir <s.mohamedaashir@gmail.com>
2024-07-06 17:51:35 +0530

    Pallindrome

--- JAVA/Hello.java
+++ JAVA/Hello.java
@@ -6,18 +6,19 @@ public class Hello {
     private static IntUnaryOperator Function = null;
 
     public static void main(String[] args) {
-        // System.out.print("Enter a number: ");
-        // int Number = input.nextInt();
-        int Number = 20;
-        int[] Fibo = new int[Number];
-        Fibo[0] = 0;
-        Fibo[1] = 1;
-        Function = n -> (n < 2) ? Fibo[n] : (Fibo[n] = Function.applyAsInt(n - 1) + Function.applyAsInt(n - 2));
-        Function.applyAsInt(Number - 1);
-        int sum = 0;
-        for (int i : Fibo)
-            sum += i;
-        System.out.printf("The average of %d Fibonacci numbers is %f", Number, (float) sum / Number);
+        System.out.print("Enter a number: ");
+        int Number = input.nextInt();
+        int Org = Number;
+        int Rev = 0;
+        while (Number > 0) {
+            Rev = (Rev * 10 + Number % 10);
+            Number /= 10;
+        }
+        if (Rev == Org) {
+            System.out.print("The number is a palindrome");
+        } else {
+            System.out.print("The number is not a palindrome");
+        }
         System.out.println();
     }
 }
