ae62d0970db153965dcd61fec0cafd72ffe3c82d
mdaashir <s.mohamedaashir@gmail.com>
2024-07-07 21:38:20 +0530

    ISBN

--- JAVA/Hello.java
+++ JAVA/Hello.java
@@ -8,52 +8,19 @@ public class Hello {
     private static BiFunction<StringBuilder, Integer, Integer> Functions = null;
 
     public static void main(String[] args) {
+        System.out.print("Enter the first 9 digits of the ISBN: ");
+        String first9String = input.nextLine();
+        StringBuilder Output = new StringBuilder(first9String);
 
-        String[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-        int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
-
-        while (true) {
-            System.out.print("Enter a number (99 to exit): ");
-            int Number = input.nextInt();
-            if (Number == 99)
-                break;
-
-            StringBuilder Roman = new StringBuilder();
-            for (int i = 0; i < values.length && Number > 0; i++) {
-                while (Number >= values[i]) {
-                    Roman.append(symbols[i]);
-                    Number -= values[i];
-                }
-            }
-            System.out.println("Roman numeral: " + Roman);
-        }
-
-        // System.out.print("Enter the first 9 digits of the ISBN: ");
-        // String first9String = input.nextLine();
-        // StringBuilder Output = new StringBuilder(first9String);
-
-        // Functions = (str, index) -> {
-        // if (index < 0)
-        // return 0;
-        // int digit = str.charAt(index) - '0';
-        // System.out.println(digit);
-        // return (digit * (index + 1)) + Functions.apply(str, index - 1);
-        // };
-        // int sum = Functions.apply(Output.reverse(), Output.length() - 1);
-        // System.out.println("Total sum: " + sum);
-        // int check = (11 - (sum % 11)) % 11;
-        // int checksum = (check == 11) ? 0 : check;
-        // String isbnLastDigit = (check == 10) ? "X" : String.valueOf(check);
-        // System.out.println("The ISBN-10 is " + first9String + isbnLastDigit);
-        // int sum = 0;
-        // for (int i = 0; i < Output.length(); i++) {
-        // int digit = Output.charAt(i) - '0';
-        // sum += digit * (i + 1);
-        // System.out.println(sum);
-        // }
-        // System.out.println("Enter the number: ");
-        // int Number = input.nextInt();
-        // System.out.println("Enter number per line: ");
-        // int Line = input.nextInt();
+        Functions = (str, index) -> {
+            if (index < 0)
+                return 0;
+            int digit = str.charAt(index) - '0';
+            return (digit * (index + 2)) + Functions.apply(str, index - 1);
+        };
+        int sum = Functions.apply(Output.reverse(), Output.length() - 1);
+        String LastDigit = (((11 - (sum % 11)) == 10) ? "X" : String.valueOf(11 - (sum % 11)));
+        System.out.printf("The ISBN-10 is %09d%s", Integer.valueOf(first9String), LastDigit);
+        System.out.println();
     }
 }
