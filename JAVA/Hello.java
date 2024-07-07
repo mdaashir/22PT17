7828edeac50c6b1facfcae6d38e591b08112836d
mdaashir <s.mohamedaashir@gmail.com>
2024-07-07 10:31:51 +0530

    CozaLozaWoza

--- JAVA/Hello.java
+++ JAVA/Hello.java
@@ -1,37 +1,57 @@
 import java.util.Scanner;
+import java.util.function.BiFunction;
 import java.util.function.IntUnaryOperator;
 
 public class Hello {
     private static Scanner input = new Scanner(System.in);
     private static IntUnaryOperator Function = null;
+    private static BiFunction<StringBuilder, Integer, Integer> Functions = null;
 
     public static void main(String[] args) {
-        System.out.print("Enter the monthly salary and age: $");
-        float salary = input.nextFloat();
-        int age = input.nextInt();
-        float employeeRate = 0;
-        float employerRate = 0;
-        if (age <= 55) {
-            employeeRate = 20.0f;
-            employerRate = 17.0f;
-        } else if (age > 55 && age <= 60) {
-            employeeRate = 13.0f;
-            employerRate = 13.0f;
-        } else if (age > 60 && age <= 65) {
-            employeeRate = 7.5f;
-            employerRate = 9.0f;
-        } else if (age > 65) {
-            employeeRate = 5.0f;
-            employerRate = 7.5f;
-        } else {
-            System.out.println("Invalid age entered.");
-            return;
+        // System.out.print("Enter the first 9 digits of the ISBN: ");
+        // String first9String = input.nextLine();
+        // StringBuilder Output = new StringBuilder(first9String);
+
+        // Functions = (str, index) -> {
+        // if (index < 0)
+        // return 0;
+        // int digit = str.charAt(index) - '0';
+        // System.out.println(digit);
+        // return (digit * (index + 1)) + Functions.apply(str, index - 1);
+        // };
+        // int sum = Functions.apply(Output.reverse(), Output.length() - 1);
+        // System.out.println("Total sum: " + sum);
+        // int check = (11 - (sum % 11)) % 11;
+        // int checksum = (check == 11) ? 0 : check;
+        // String isbnLastDigit = (check == 10) ? "X" : String.valueOf(check);
+        // System.out.println("The ISBN-10 is " + first9String + isbnLastDigit);
+        // int sum = 0;
+        // for (int i = 0; i < Output.length(); i++) {
+        // int digit = Output.charAt(i) - '0';
+        // sum += digit * (i + 1);
+        // System.out.println(sum);
+        // }
+        // System.out.println("Enter the number: ");
+        // int Number = input.nextInt();
+        // System.out.println("Enter number per line: ");
+        // int Line = input.nextInt();
+        int Number = 110, Line = 11;
+        for (int i = 1; i <= Number; i++) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+                if (i % 3 == 0)
+                    System.out.print("Coza");
+                if (i % 5 == 0)
+                    System.out.print("Loza");
+                if (i % 7 == 0)
+                    System.out.print("Woza");
+                System.out.printf("%-6s", "");
+            } else {
+                System.out.printf("%-6d", i);
+            }
+
+            if (i % Line == 0) {
+                System.out.println();
+            }
         }
-        float cappedSalary = Math.min(salary, 6000.0f);
-        float employeeContribution = (employeeRate / 100f) * cappedSalary;
-        float employerContribution = (employerRate / 100f) * cappedSalary;
-        System.out.printf("The employee's contribution is: $%.2f\n", employeeContribution);
-        System.out.printf("The employer's contribution is: $%.2f\n", employerContribution);
-        System.out.printf("The total contribution is: $%.2f\n", employeeContribution + employerContribution);
     }
 }
