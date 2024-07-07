66d367bb6b68a7f56700d0312afa628dc3ed3a83
mdaashir <s.mohamedaashir@gmail.com>
2024-07-07 07:17:00 +0530

    Employee commit

--- JAVA/Main.java
+++ JAVA/Main.java
@@ -20,6 +20,7 @@ public class Main {
         DataPlan();
         NetPay();
         HarmonicSum();
+        Employee();
     }
 
     private static void Factorial() {
@@ -288,5 +289,35 @@ public class Main {
         System.out.println("Sum from left to right: " + sumLTR);
         System.out.println("Sum from right to left: " + sumRTL);
         System.out.println("Absolute difference: " + Math.abs(sumLTR - sumRTL));
+    }
+
+    public static void Employee() {
+        System.out.print("Enter the monthly salary and age: $");
+        float salary = input.nextFloat();
+        int age = input.nextInt();
+        float employeeRate = 0;
+        float employerRate = 0;
+        if (age <= 55) {
+            employeeRate = 20.0f;
+            employerRate = 17.0f;
+        } else if (age > 55 && age <= 60) {
+            employeeRate = 13.0f;
+            employerRate = 13.0f;
+        } else if (age > 60 && age <= 65) {
+            employeeRate = 7.5f;
+            employerRate = 9.0f;
+        } else if (age > 65) {
+            employeeRate = 5.0f;
+            employerRate = 7.5f;
+        } else {
+            System.out.println("Invalid age entered.");
+            return;
+        }
+        float cappedSalary = Math.min(salary, 6000.0f);
+        float employeeContribution = (employeeRate / 100f) * cappedSalary;
+        float employerContribution = (employerRate / 100f) * cappedSalary;
+        System.out.printf("The employee's contribution is: $%.2f\n", employeeContribution);
+        System.out.printf("The employer's contribution is: $%.2f\n", employerContribution);
+        System.out.printf("The total contribution is: $%.2f\n", employeeContribution + employerContribution);
     }
 }
