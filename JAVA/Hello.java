80500ce63288ed206b4f81ffc007fa66ae606285
mdaashir <s.mohamedaashir@gmail.com>
2024-07-07 06:45:50 +0530

    HarmonicSum

--- JAVA/Hello.java
+++ JAVA/Hello.java
@@ -1,10 +1,8 @@
-import java.util.Scanner;
 import java.util.function.BiFunction;
-import java.util.function.IntUnaryOperator;
 
 public class Hello {
-    private static Scanner input = new Scanner(System.in);
-    private static IntUnaryOperator Function = null;
+    // private static Scanner input = new Scanner(System.in);
+    // private static IntUnaryOperator Function = null;
 
     public static void main(String[] args) {
         // System.out.print("Enter a number: ");
