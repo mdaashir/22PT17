58d96e68fdddfd5220e794a13053205ec9694ffb
mdaashir <s.mohamedaashir@gmail.com>
2024-07-08 01:28:10 +0530

    initial commit

--- JAVA/Hello.java
+++ JAVA/Hello.java
@@ -8,19 +8,8 @@ public class Hello {
     private static BiFunction<StringBuilder, Integer, Integer> Functions = null;
 
     public static void main(String[] args) {
-        System.out.print("Enter the first 9 digits of the ISBN: ");
-        String first9String = input.nextLine();
-        StringBuilder Output = new StringBuilder(first9String);
-
-        Functions = (str, index) -> {
-            if (index < 0)
-                return 0;
-            int digit = str.charAt(index) - '0';
-            return (digit * (index + 2)) + Functions.apply(str, index - 1);
-        };
-        int sum = Functions.apply(Output.reverse(), Output.length() - 1);
-        String LastDigit = (((11 - (sum % 11)) == 10) ? "X" : String.valueOf(11 - (sum % 11)));
-        System.out.printf("The ISBN-10 is %09d%s", Integer.valueOf(first9String), LastDigit);
-        System.out.println();
+        System.out.print("Enter a Number: ");
+        int Number = input.nextInt();
+        System.out.println(Number);
     }
 }
