ff0e95a722759cee7aaaef60bb87ba8060c37fcf
mdaashir <s.mohamedaashir@gmail.com>
2024-07-15 18:00:44 +0530

    root main intial commit

--- JAVA/Main.java
+++ JAVA/Main.java
@@ -1,31 +1,16 @@
 import java.util.Scanner;
-import java.util.function.BiFunction;
-import java.util.function.IntUnaryOperator;
+// import java.util.function.BiFunction;
+// import java.util.function.IntUnaryOperator;
 
 public class Main {
     private static Scanner input = new Scanner(System.in);
-    private static IntUnaryOperator Function = null;
-    private static BiFunction<StringBuilder, Integer, Integer> Functions = null;
+    // private static IntUnaryOperator Function = null;
+    // private static BiFunction<StringBuilder, Integer, Integer> Functions = null;
 
     public static void main(String[] args) {
         System.out.print("Enter a number of elements: ");
         int Number = input.nextInt();
-        int[] Array = new int[Number];
-        System.out.printf("Enter a values: ");
-        for (int i = 0; i < Number; i++) {
-            Array[i] = input.nextInt();
-        }
-        System.out.print("Stored Array before reverse: ");
-        for (int i : Array)
-            System.out.print(i + " ");
-        for (int i = 0; i < Number / 2; i++) {
-            int temp = Array[i];
-            Array[i] = Array[Number - i - 1];
-            Array[Number - i - 1] = temp;
-        }
-        System.out.printf("%nStored Array after reverse: ");
-        for (int i : Array)
-            System.out.print(i + " ");
+        System.out.print(Number);
         System.out.println();
     }
 }
