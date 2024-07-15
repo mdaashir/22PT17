2410351c6529814ac0c415d73bb0599956e6fe72
mdaashir <s.mohamedaashir@gmail.com>
2024-07-15 18:02:32 +0530

    import packages for ps3 main

--- JAVA/Temp.java
+++ JAVA/Temp.java
@@ -1,4 +1,11 @@
+import Problem_sheet_3.Main;
+
 public class Temp {
+    Temp() {
+        System.out.println("Hello");
+    }
+
     public static void main(String[] args) {
+        Main.main(1);
     }
 }
