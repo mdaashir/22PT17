9fd83163f8adafe6d4bf24469e402eea5fec1f56
mdaashir <s.mohamedaashir@gmail.com>
2024-07-15 06:48:01 +0530

    array reverse logic addded

--- JAVA/Hello.java
+++ JAVA/Hello.java
@@ -8,8 +8,24 @@ public class Hello {
     private static BiFunction<StringBuilder, Integer, Integer> Functions = null;
 
     public static void main(String[] args) {
-        System.out.print("Enter a Number: ");
+        System.out.print("Enter a number of elements: ");
         int Number = input.nextInt();
-        System.out.println(Number);
+        int[] Array = new int[Number];
+        System.out.printf("Enter a values: ");
+        for (int i = 0; i < Number; i++) {
+            Array[i] = input.nextInt();
+        }
+        System.out.print("Stored Array before reverse: ");
+        for (int i : Array)
+            System.out.print(i + " ");
+        for (int i = 0; i < Number / 2; i++) {
+            int temp = Array[i];
+            Array[i] = Array[Number - i - 1];
+            Array[Number - i - 1] = temp;
+        }
+        System.out.printf("%nStored Array after reverse: ");
+        for (int i : Array)
+            System.out.print(i + " ");
+        System.out.println();
     }
 }
