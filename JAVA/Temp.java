89ee61a6400790bc15c7cfeef480a4d74f03beca
mdaashir <s.mohamedaashir@gmail.com>
2024-07-29 19:31:51 +0530

    feat: Add user input for Question Number in Temp.java

--- JAVA/Temp.java
+++ JAVA/Temp.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Temp {
 
     public static void main(String[] args) {
-        new Problem_Sheet_3(1);
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter the Question Number: ");
+            new Problem_Sheet_3(input.nextInt());
+        }
     }
 }
